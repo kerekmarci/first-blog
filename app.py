cf094f7dcdfb60ae7d53fdab0810bae6a5188b1a
kerekmarci <kerekmarci@gmail.com>
2021-05-22 20:58:32 +0000

    Editing functionality added

--- app.py
+++ app.py
@@ -18,47 +18,58 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', page_title="Home page")
+@app.route("/")
+def index():    
+    return render_template("index.html", page_title="Home page")
 
 
-@app.route('/blog')
+@app.route("/blog")
 def blog():
     blogposts = mongo.db.blog_posts.find()
-    return render_template('blog.html', page_title="Blog posts", blogposts=blogposts)
+    return render_template("blog.html", page_title="Blog posts", blogposts=blogposts)
 
 
-@app.route('/add')
-def add():
-    return render_template('add.html', page_title="Add a New Blog")
-
-
-@app.route('/postblog', methods=["GET", "POST"])
+@app.route("/postblog", methods=["GET", "POST"])
 def postblog():
     if request.method == "POST":
         newblog = {
             "title": request.form.get("title"),
             "subtitle": request.form.get("subtitle"),
             "author": request.form.get("author"),
-            "content": request.form.get("blogcontent")
+            "content": request.form.get("content")
         }
 
         mongo.db.blog_posts.insert(newblog)
         flash("Blog successfully posted!")
         return redirect(url_for("blog"))
 
-    return render_template("add.html")
+    return render_template("postblog.html", page_title="Write a new blog!")
 
 
-@app.route('/about')
+@app.route("/edit_blog/<blog_id>", methods=["GET", "POST"])
+def edit_blog(blog_id):
+    if request.method == "POST":
+        blog = {
+            "title": request.form.get("title"),
+            "subtitle": request.form.get("subtitle"),
+            "author": request.form.get("author"),
+            "content": request.form.get("content")
+        }
+        mongo.db.blog_posts.update({"_id": ObjectId(blog_id)}, blog)
+        flash("Blog Successfully Updated")
+
+    blogpost = mongo.db.blog_posts.find_one({"_id": ObjectId(blog_id)})       
+    return render_template("edit_blog.html", page_title="Edit Blog", blogpost=blogpost)
+
+
+@app.route("/about")
 def about():
-    return render_template('about.html', page_title="About me & this page")
+    return render_template("about.html", page_title="About me & this page")
 
 
-@app.route('/contact')
+@app.route("/contact")
 def contact():
-    return render_template('contact.html', page_title="Contact me")
+    return render_template("contact.html", page_title="Contact me")
 
 
 if __name__ == '__main__':
