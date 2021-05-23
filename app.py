76b2531a8b7dcd298b594e577c40cb4bf0f5bcdd
kerekmarci <kerekmarci@gmail.com>
2021-05-23 11:15:25 +0000

    Log in and Registration functionality added

--- app.py
+++ app.py
@@ -57,9 +57,63 @@ def edit_blog(blog_id):
         }
         mongo.db.blog_posts.update({"_id": ObjectId(blog_id)}, blog)
         flash("Blog Successfully Updated")
+        return redirect(url_for("blog"))
 
     blogpost = mongo.db.blog_posts.find_one({"_id": ObjectId(blog_id)})       
     return render_template("edit_blog.html", page_title="Edit Blog", blogpost=blogpost)
+
+
+@app.route("/delete_blog/<blog_id>")
+def delete_blog(blog_id):
+    mongo.db.blog_posts.remove({"_id": ObjectId(blog_id)})
+    flash("Blog Successfully Deleted")
+    return redirect(url_for("blog"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower() })
+
+        if existing_user:
+            flash("Username already exists. Try to log in")
+            return redirect(url_for("login"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("blog", username=session["user"]))
+    return render_template("register.html", page_title="Register a new account")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower() })
+        
+        if existing_user:
+            # ensure hashed password matches input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome. {}".format(request.form.get("username")))
+                    return redirect(url_for("blog"))
+            else: 
+                flash("Incorrect username or password")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist
+            flash("Username doesn't exist")
+            return redirect(url_for("login"))
+
+    return render_template("login.html", page_title="Log in")
 
 
 @app.route("/about")
